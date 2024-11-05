91af432a773e8dd0f6efb1ef90a904e862ca8654
walkawayy <81546780+walkawayy@users.noreply.github.com>
2024-11-05 11:10:22 -0500

    update save counter when using the console

--- src/tr2/game/savegame/common.c
+++ src/tr2/game/savegame/common.c
@@ -17,7 +17,11 @@ bool Savegame_IsSlotFree(const int32_t slot_idx)
 bool Savegame_Save(const int32_t slot_idx)
 {
     CreateSaveGameInfo();
+    const int16_t slot_num = g_Inv_ExtraData[1];
     S_SaveGame(&g_SaveGame, sizeof(SAVEGAME_INFO), slot_idx);
+    // TODO: move me inside S_SaveGame
+    g_SaveGameReqFlags1[slot_num] = g_RequesterFlags1[slot_num];
+    g_SaveGameReqFlags2[slot_num] = g_RequesterFlags2[slot_num];
     GetSavedGamesList(&g_LoadGameRequester);
     return true;
 }
